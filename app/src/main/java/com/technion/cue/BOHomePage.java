23b8999aabad418dfc1b90826f5073f206d95349
Ophir Eyal <ophirey@gmail.com>
2019-12-06 17:21:18 +0200

    added logo, name & description to BO homepage ; only front-end for now

--- app/src/main/java/com/technion/cue/BOHomePage.java
+++ app/src/main/java/com/technion/cue/BOHomePage.java
@@ -3,6 +3,7 @@ package com.technion.cue;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class BOHomePage extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class BOHomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bohome_page);
+
     }
+
+
+
 }
