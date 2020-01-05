7bdfefbf16a6abb856966796a59eb4f89eb705d7
Ophir Eyal <ophirey@gmail.com>
2020-01-05 22:26:50 +0200

    finished registration revision

--- app/src/main/java/com/technion/cue/BusinessFeatures/BusinessSignUp3.java
+++ app/src/main/java/com/technion/cue/BusinessFeatures/BusinessSignUp3.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.text.SimpleDateFormat;
@@ -17,6 +18,8 @@ import java.text.SimpleDateFormat;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseUser;
 import com.technion.cue.R;
@@ -32,8 +35,6 @@ import static com.technion.cue.FirebaseCollections.BUSINESSES_COLLECTION;
 @ModuleAuthor("Topaz & Ophir")
 public class BusinessSignUp3 extends Fragment {
     private final BusinessSignUpContainer businessSignUpContainer;
-    private String[] days =
-            {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
     private String lastUsedKey;
 
     BusinessSignUp3(BusinessSignUpContainer businessSignUpContainer) {
@@ -53,157 +54,184 @@ public class BusinessSignUp3 extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        RecyclerView daysList = view.findViewById(R.id.schedule_days_sign_up);
 
-        for (String day : days) {
-            businessSignUpContainer.open_hours.put(day, "");
-        }
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        daysList.setLayoutManager(layoutManager);
 
-        AutoCompleteTextView openHoursDays = view.findViewById(R.id.days_filled_exposed_dropdown);
-        openHoursDays.setAdapter(new ArrayAdapter<>(getActivity(),
-                R.layout.dropdown_menu_popup_item, days));
+        daysList.setHasFixedSize(true);
 
-        TextView openHours = view.findViewById(R.id.openingHoursEditText);
-        TextView closeHours = view.findViewById(R.id.closingHoursEditText);
-        openHours.setClickable(false);
-        closeHours.setClickable(false);
-
-        openHoursDays.setOnItemClickListener((parent, v, position, id) -> {
-
-            // we use the map open_hours in order to store the operating hours of the business
-            // we check if the chosen day's operating hours are already stored in the map
-            // if they aren't, we store them. Regardless, we set the opening hour and closing hour
-            // text fields content according to this day's stored values.
-            String key = parent.getItemAtPosition(position).toString();
-            if (businessSignUpContainer.open_hours.containsKey(key)) {
-                String open_hour = "", close_hour = "";
-                String hours = businessSignUpContainer.open_hours.get(key);
-                if (hours.contains("-")) {
-                    open_hour = hours.split("-")[0];
-                    close_hour = hours.split("-")[1];
-                }
-                openHours.setText(open_hour);
-                closeHours.setText(close_hour);
-                lastUsedKey = key;
-            }
-
-            openHours.setClickable(true);
-            closeHours.setClickable(true);
-
-        });
-
-        // setting the opening hours of the business. we use a TimePickerDialog to let
-        // the business owner to choose a new opening time, which will be displayed in the
-        // opening hour text field.
-        // Additionally, we make sure that the chosen opening time precedes the chosen closing time.
-        openHours.setOnClickListener(cl -> {
-            int hour, minute;
-            Calendar c = Calendar.getInstance();
-            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-            try {
-                Date d = sdf.parse(openHours.getText().toString());
-                c.setTime(d);
-                hour = c.get(Calendar.HOUR_OF_DAY);
-                minute = c.get(Calendar.MINUTE);
-            } catch (ParseException e) {
-                hour = c.get(Calendar.HOUR_OF_DAY);
-                minute = c.get(Calendar.MINUTE);
-            }
-
-            TimePickerDialog mTimePicker = new TimePickerDialog(getActivity(),
-                    (timePicker, selectedHour, selectedMinute) -> {
-
-                        String closeHoursText = closeHours.getText().toString();
-                        if (closeHoursText.contains(":")) {
-                            String closeHour = closeHoursText.split(":")[0];
-                            String closeMinute = closeHoursText.split(":")[1];
-                            if ((selectedHour > Integer.valueOf(closeHour)) ||
-                                    (selectedHour == Integer.valueOf(closeHour)
-                                            && selectedMinute >= Integer.valueOf(closeMinute))) {
-                                Toast.makeText(getActivity(),
-                                        "Opening time must precede closing time. Please refill the form.",
-                                        Toast.LENGTH_SHORT).show();
-                                return;
-                            }
-                        }
-
-                        String selectedMinuteFormatted =
-                                selectedMinute == 0 ? "00" : String.valueOf(selectedMinute);
-                        String selectedHourFormatted =
-                                selectedHour <= 9 ? "0" + selectedHour : String.valueOf(selectedHour);
-                        String value_to_put = selectedHourFormatted + ":" + selectedMinuteFormatted;
-
-                        openHours.setText(value_to_put);
-                        if (lastUsedKey == null)
-                            return;
-                        if (businessSignUpContainer.open_hours.get(lastUsedKey).contains("-")) {
-                            businessSignUpContainer.open_hours.put(lastUsedKey, value_to_put + "-" +
-                                    businessSignUpContainer.open_hours
-                                            .get(lastUsedKey).split("-")[1]);
-                        } else {
-                            businessSignUpContainer.open_hours.put(lastUsedKey, value_to_put + "-");
-                        }
-                    }, hour, minute, true);
-            mTimePicker.setTitle("Select opening hour");
-            mTimePicker.show();
-        });
-
-        // same as above, but for the closing time
-        closeHours.setOnClickListener(cl -> {
-            int hour, minute;
-            Calendar c = Calendar.getInstance();
-            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-            try {
-                Date d = sdf.parse(openHours.getText().toString());
-                c.setTime(d);
-                hour = c.get(Calendar.HOUR_OF_DAY);
-                minute = c.get(Calendar.MINUTE);
-            } catch (ParseException e) {
-                hour = c.get(Calendar.HOUR_OF_DAY);
-                minute = c.get(Calendar.MINUTE);
-            }
-
-            TimePickerDialog mTimePicker = new TimePickerDialog(getActivity(),
-                    (timePicker, selectedHour, selectedMinute) -> {
-
-                        String openHoursText = openHours.getText().toString();
-                        if (openHoursText.contains(":")) {
-                            String openHour = openHoursText.split(":")[0];
-                            String openMinute = openHoursText.split(":")[1];
-                            if ((selectedHour < Integer.valueOf(openHour)) ||
-                                    (selectedHour == Integer.valueOf(openHour)
-                                            && selectedMinute <= Integer.valueOf(openMinute))) {
-                                Toast.makeText(getActivity(),
-                                        "Opening time must precede closing time. Please refill the form.",
-                                        Toast.LENGTH_SHORT).show();
-                                return;
-                            }
-                        }
-
-                        String selectedMinuteFormatted =
-                                selectedMinute == 0 ? "00" : String.valueOf(selectedMinute);
-                        String selectedHourFormatted =
-                                selectedHour <= 9 ? "0" + selectedHour : String.valueOf(selectedHour);
-                        String value_to_put = selectedHourFormatted + ":" + selectedMinuteFormatted;
-                        closeHours.setText(value_to_put);
-                        if (lastUsedKey == null)
-                            return;
-                        if (businessSignUpContainer.open_hours.get(lastUsedKey).contains("-")) {
-                            businessSignUpContainer.open_hours.put(lastUsedKey,
-                                    businessSignUpContainer.open_hours.get(lastUsedKey)
-                                            .split("-")[0] + "-" + value_to_put);
-                        } else {
-                            businessSignUpContainer.open_hours
-                                    .put(lastUsedKey, "-" + value_to_put);
-                        }
-                    }, hour, minute, true);
-            mTimePicker.setTitle("Select closing hour");
-            mTimePicker.show();
-        });
+        daysList.setAdapter(new OpenDaysAdapter());
 
         businessSignUpContainer.done_btn = view.findViewById(R.id.sign_up_finish_button);
 
         businessSignUpContainer.done_btn
                 .setOnClickListener(v -> businessSignUpContainer.onFinished());
+    }
+
+    class OpenDaysAdapter extends RecyclerView.Adapter<OpenDaysAdapter.DayHolder> {
+
+        private String[] days =
+                {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+        class DayHolder extends RecyclerView.ViewHolder {
+            public String dayText;
+            TextView day;
+            TextView openHours;
+            TextView closeHours;
+
+            DayHolder(@NonNull View itemView) {
+                super(itemView);
+                openHours = itemView.findViewById(R.id.openingHoursEditText);
+                closeHours = itemView.findViewById(R.id.closingHoursEditText);
+                day = itemView.findViewById(R.id.sign_up_day);
+
+                for (String day : days) {
+                    if (!businessSignUpContainer.open_hours.containsKey(day))
+                        businessSignUpContainer.open_hours.put(day, "");
+                }
+
+                // setting the opening hours of the business. we use a TimePickerDialog to let
+                // the business owner to choose a new opening time, which will be displayed in the
+                // opening hour text field.
+                // Additionally, we make sure that the chosen opening time precedes the chosen closing time.
+                openHours.setOnClickListener(cl -> {
+                    int hour, minute;
+                    Calendar c = Calendar.getInstance();
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                    try {
+                        Date d = sdf.parse(openHours.getText().toString());
+                        c.setTime(d);
+                        hour = c.get(Calendar.HOUR_OF_DAY);
+                        minute = c.get(Calendar.MINUTE);
+                    } catch (ParseException e) {
+                        hour = c.get(Calendar.HOUR_OF_DAY);
+                        minute = c.get(Calendar.MINUTE);
+                    }
+
+                    TimePickerDialog mTimePicker = new TimePickerDialog(getActivity(),
+                            (timePicker, selectedHour, selectedMinute) -> {
+
+                                String closeHoursText = closeHours.getText().toString();
+                                if (closeHoursText.contains(":")) {
+                                    String closeHour = closeHoursText.split(":")[0];
+                                    String closeMinute = closeHoursText.split(":")[1];
+                                    if ((selectedHour > Integer.valueOf(closeHour)) ||
+                                            (selectedHour == Integer.valueOf(closeHour)
+                                                    && selectedMinute >= Integer.valueOf(closeMinute))) {
+                                        Toast.makeText(getActivity(),
+                                                "Opening time must precede closing time. Please refill the form.",
+                                                Toast.LENGTH_SHORT).show();
+                                        return;
+                                    }
+                                }
+
+                                String selectedMinuteFormatted =
+                                        selectedMinute == 0 ? "00" : String.valueOf(selectedMinute);
+                                String selectedHourFormatted =
+                                        selectedHour <= 9 ? "0" + selectedHour : String.valueOf(selectedHour);
+                                String value_to_put = selectedHourFormatted + ":" + selectedMinuteFormatted;
+
+                                openHours.setText(value_to_put);
+                                if (businessSignUpContainer.open_hours.get(dayText).contains("-")) {
+                                    businessSignUpContainer.open_hours.put(dayText, value_to_put + "-" +
+                                            businessSignUpContainer.open_hours
+                                                    .get(dayText).split("-")[1]);
+                                } else {
+                                    businessSignUpContainer.open_hours.put(dayText, value_to_put + "-");
+                                }
+                            }, hour, minute, true);
+                    mTimePicker.setTitle("Select opening hour");
+                    mTimePicker.show();
+                });
+
+                // same as above, but for the closing time
+                closeHours.setOnClickListener(cl -> {
+                    int hour, minute;
+                    Calendar c = Calendar.getInstance();
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                    try {
+                        Date d = sdf.parse(openHours.getText().toString());
+                        c.setTime(d);
+                        hour = c.get(Calendar.HOUR_OF_DAY);
+                        minute = c.get(Calendar.MINUTE);
+                    } catch (ParseException e) {
+                        hour = c.get(Calendar.HOUR_OF_DAY);
+                        minute = c.get(Calendar.MINUTE);
+                    }
+
+                    TimePickerDialog mTimePicker = new TimePickerDialog(getActivity(),
+                            (timePicker, selectedHour, selectedMinute) -> {
+
+                                String openHoursText = openHours.getText().toString();
+                                if (openHoursText.contains(":")) {
+                                    String openHour = openHoursText.split(":")[0];
+                                    String openMinute = openHoursText.split(":")[1];
+                                    if ((selectedHour < Integer.valueOf(openHour)) ||
+                                            (selectedHour == Integer.valueOf(openHour)
+                                                    && selectedMinute <= Integer.valueOf(openMinute))) {
+                                        Toast.makeText(getActivity(),
+                                                "Opening time must precede closing time. Please refill the form.",
+                                                Toast.LENGTH_SHORT).show();
+                                        return;
+                                    }
+                                }
+
+                                String selectedMinuteFormatted =
+                                        selectedMinute == 0 ? "00" : String.valueOf(selectedMinute);
+                                String selectedHourFormatted =
+                                        selectedHour <= 9 ? "0" + selectedHour : String.valueOf(selectedHour);
+                                String value_to_put = selectedHourFormatted + ":" + selectedMinuteFormatted;
+                                closeHours.setText(value_to_put);
+                                if (businessSignUpContainer.open_hours.get(dayText).contains("-")) {
+                                    businessSignUpContainer.open_hours.put(dayText,
+                                            businessSignUpContainer.open_hours.get(dayText)
+                                                    .split("-")[0] + "-" + value_to_put);
+                                } else {
+                                    businessSignUpContainer.open_hours
+                                            .put(dayText, "-" + value_to_put);
+                                }
+                            }, hour, minute, true);
+                    mTimePicker.setTitle("Select closing hour");
+                    mTimePicker.show();
+                });
+            }
+        }
+
+        @NonNull
+        @Override
+        public OpenDaysAdapter.DayHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new DayHolder(LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.sign_up_day_holder, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull OpenDaysAdapter.DayHolder holder, int position) {
+            holder.day.setText(days[position]);
+            holder.dayText = days[position];
+
+            String set_hours = businessSignUpContainer.open_hours.get(days[position]);
+            if (set_hours.contains("-")) {
+                if (set_hours.split("-").length == 2) {
+                    holder.openHours.setText(set_hours.split("-")[0]);
+                    holder.closeHours.setText(set_hours.split("-")[1]);
+                } else if (set_hours.split("-")[0].contains(":")) {
+                    holder.openHours.setText(set_hours.split("-")[0]);
+                } else {
+                    holder.closeHours.setText(set_hours.split("-")[1]);
+                }
+            } else {
+                holder.openHours.setText("");
+                holder.closeHours.setText("");
+            }
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 7;
+        }
     }
 }
 
