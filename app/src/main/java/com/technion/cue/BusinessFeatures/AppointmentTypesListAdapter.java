cf7c78c33909ac04b81e9aa89214ea6359e0cb60
topaz4 <33227202+topaz4@users.noreply.github.com>
2020-01-15 19:07:57 +0200

    deleted un important imports

--- app/src/main/java/com/technion/cue/BusinessFeatures/AppointmentTypesListAdapter.java
+++ app/src/main/java/com/technion/cue/BusinessFeatures/AppointmentTypesListAdapter.java
@@ -1,42 +1,16 @@
 package com.technion.cue.BusinessFeatures;
 
-
-import android.content.Context;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.core.util.Pair;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.TextView;
-import android.widget.AdapterView.OnItemClickListener;
 import androidx.annotation.NonNull;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.technion.cue.R;
-import com.technion.cue.data_classes.Business.AppointmentType;
-
-import static com.technion.cue.FirebaseCollections.BUSINESSES_COLLECTION;
-import static com.technion.cue.FirebaseCollections.TYPES_COLLECTION;
-
-
-
 import com.technion.cue.R;
 import com.technion.cue.data_classes.Business.AppointmentType;
 
